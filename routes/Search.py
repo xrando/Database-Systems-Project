f23b356fccfd7c987d4f260d76536ea90b10374a
Benjamin Loh <xrando20@gmail.com>
2023-06-19 21:51:13 +0800

    Combined search filters
    Combined search filters

--- routes/Search.py
+++ routes/Search.py
@@ -12,22 +12,26 @@ config = config_manager.get_config()
 
 @routes.route('/search', methods=['POST'])
 def search():
-    choice = request.form['choice']
+    #choice = request.form['choice']
     query = request.form['search']
-    if choice == 'director':
-        # search for directors
-        results = DBMS_Movie.search_directors(query)
-        print(results)
-    elif choice == 'actor':
-        # search for actors
-        results = DBMS_Movie.search_actors(query)
-    elif choice == 'movie':
-        # search for movie
-        results = DBMS_Movie.search_movies(query)
-    elif choice == 'profile':
-        # search for user profile
-        results = dbUser.search_user(query)
-    else:
-        # Handle invalid choice
-        results = []
-    return render_template('search.html', results=results, choice=choice)
+    # if choice == 'director':
+    #     # search for directors
+    #     results = DBMS_Movie.search_directors(query)
+    #     print(results)
+    # elif choice == 'actor':
+    #     # search for actors
+    #     results = DBMS_Movie.search_actors(query)
+    # elif choice == 'movie':
+    #     # search for movie
+    #     results = DBMS_Movie.search_movies(query)
+    # elif choice == 'profile':
+    #     # search for user profile
+    #     results = dbUser.search_user(query)
+    # else:
+    #     # Handle invalid choice
+    #     results = []
+    director_results = DBMS_Movie.search_directors(query)
+    actor_results = DBMS_Movie.search_actors(query)
+    movie_results = DBMS_Movie.search_movies(query)
+    profile_results = dbUser.search_user(query)
+    return render_template('search.html', directors=director_results, actors=actor_results, movies=movie_results, profiles=profile_results)
