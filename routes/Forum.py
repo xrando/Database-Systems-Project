3b3b7a5f1d2325f13576f00304b63dd3a8cff194
Benjamin Loh <xrando20@gmail.com>
2023-06-21 17:01:22 +0800

    added comments feature for posts
    added comments feature for posts

--- routes/Forum.py
+++ routes/Forum.py
@@ -1,3 +1,4 @@
+from bson import ObjectId
 from flask import render_template, request
 from flask_login import current_user
 from . import routes
@@ -12,54 +13,59 @@ config_manager = ConfigManager()
 config = config_manager.get_config()
 handler = Mongo.MongoDBHandler(config.get('MONGODB', 'CONNECTION_STRING'), config.get('MONGODB', 'DATABASE'))
 
+@routes.route('/comment', methods=['POST'])
 @routes.route('/post', methods=['POST', 'GET'])
 def post():
+    #init
     allPostsToDisplay = []
     allPostsUsernames = []
     posts = []
-    #if post, save post to mongodb
-    if request.method == 'POST':
-        #get post data
-        subject = request.form['subject']
-        comment = request.form['comment']
-        userid = request.form['userid']
-        print(subject, comment, userid)
+    if request.path == '/post':
+        #if post, save post to mongodb
+        if request.method == 'POST':
+            #get post data
+            subject = request.form['subject']
+            comment = request.form['comment']
+            userid = request.form['userid']
+            print(subject, comment, userid)
+            #save to mongodb
+            handler.insert_document(config.get('MONGODB', 'FORUM_COLLECTION'), {
+                'subject': subject,
+                'comment': comment,
+                'userid': int(userid),
+                'replies': [],
+            })
+    else:
+        #post reply to comment
+        #get reply data
+        reply = request.form['reply']
+        postID = request.form['postid']
+        print(reply)
+        print(postID)
         #save to mongodb
-        handler.insert_document(config.get('MONGODB', 'FORUM_COLLECTION'), {
-            'subject': subject,
-            'comment': comment,
-            'userid': int(userid),
-        })
+        if handler.find_documents(config.get('MONGODB', 'FORUM_COLLECTION'), {'_id': ObjectId(postID)}):
+            handler.update_document(config.get('MONGODB', 'FORUM_COLLECTION'), {'_id': ObjectId(postID)}, {
+                'replies': reply,
+            }, '$push')
+        else:
+            print("not found")
+
+    #grab all posts from mongodb and respective user for display
+    #get user following
+    userFollows = handler.find_documents(config.get('MONGODB', 'FOLLOW_COLLECTION'), {'user_id': current_user.id})
+    if userFollows:
+        userFollows = userFollows[0]['following_arr']
+        for user in userFollows:
+            print('following: '+str(user))
+            #get all posts for each following
+            userFollowingPosts = handler.find_documents(config.get('MONGODB', 'FORUM_COLLECTION'), {'userid': user})
+            if userFollowingPosts:
+                #get username
+                name = dbUser.get_user_by_id(user)[3]
+                for post in userFollowingPosts:
+                    posts.append((name, post))
+    print(posts)
+    #print(allPostsUsernames)
+    return render_template('forum.html', posts = posts)
 
 
-    #if get, grab all posts from mongodb and match with user
-    if request.method == 'GET':
-        #get user following
-        userFollows = handler.find_documents(config.get('MONGODB', 'FOLLOW_COLLECTION'), {'user_id': current_user.id})
-        if userFollows:
-            userFollows = userFollows[0]['following_arr']
-            for user in userFollows:
-                #userFollowsName.append(dbUser.get_user_by_id(user))
-                print('following: '+str(user))
-                #get all posts for each following
-                userFollowingPosts = handler.find_documents(config.get('MONGODB', 'FORUM_COLLECTION'), {'userid': user})
-                if userFollowingPosts:
-                    #user to allPostsUsernames
-                    # print(dbUser.get_user_by_id(user))
-                    #get username
-                    name = dbUser.get_user_by_id(user)[3]
-                    if name:
-                        allPostsUsernames.append(dbUser.get_user_by_id(user)[3])
-                    #append each post to allPostsToDisplay
-                    for post in userFollowingPosts:
-                        allPostsToDisplay.append(post)
-                        print(post)
-        for user, post in zip(allPostsUsernames, allPostsToDisplay):
-            print(user, post)
-            posts.append((user, post))
-        print(allPostsUsernames)
-
-
-
-
-    return render_template('forum.html', posts = posts, usernames = allPostsUsernames)
