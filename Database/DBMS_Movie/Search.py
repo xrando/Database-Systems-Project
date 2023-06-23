ac00b97cb1d81fa148cb793d4cc6c3a6130a433f
Benjamin Loh <xrando20@gmail.com>
2023-06-23 15:27:43 +0800

    parameterized search queries
    parameterized search queries

--- Database/DBMS_Movie/Search.py
+++ Database/DBMS_Movie/Search.py
@@ -13,34 +13,57 @@ cursor = connection.cursor()
 
 
 # search
-def search_directors(name: str) -> tuple:
-    cursor.execute("SELECT * "
-                   "FROM Director "
-                   "WHERE director_name "
-                   "LIKE %s"
-                   "LIMIT 30", ('%' + name + '%',))
+def search_directors(name: str) -> tuple | None:
+    # cursor.execute("SELECT * "
+    #                "FROM Director "
+    #                "WHERE director_name "
+    #                "LIKE %s"
+    #                "LIMIT 30", ('%' + name + '%',))
+    search_stmt = "SELECT * " \
+                  "FROM Director " \
+                  "WHERE director_name " \
+                  "LIKE ? " \
+                  "LIMIT 30"
+    cursor.execute(search_stmt, ('%' + name + '%',))
     return cursor.fetchall()
 
 
-def search_movies(name: str) -> tuple:
-    cursor.execute("SELECT * "
-                   "FROM Movie "
-                   "WHERE title "
-                   "LIKE %s"
-                   "LIMIT 30", ('%' + name + '%',))
+def search_movies(name: str) -> tuple | None:
+    # cursor.execute("SELECT * "
+    #                "FROM Movie "
+    #                "WHERE title "
+    #                "LIKE %s"
+    #                "LIMIT 30", ('%' + name + '%',))
+    search_stmt = "SELECT * " \
+                  "FROM Movie " \
+                  "WHERE title " \
+                  "LIKE ? " \
+                  "LIMIT 30"
+    cursor.execute(search_stmt, ('%' + name + '%',))
     return cursor.fetchall()
 
 
-def search_actors(name: str) -> tuple:
-    cursor.execute("SELECT * "
-                   "FROM Actor "
-                   "WHERE actor_name "
-                   "LIKE %s"
-                   "LIMIT 30", ('%' + name + '%',))
+def search_actors(name: str) -> tuple | None:
+    # cursor.execute("SELECT * "
+    #                "FROM Actor "
+    #                "WHERE actor_name "
+    #                "LIKE %s"
+    #                "LIMIT 30", ('%' + name + '%',))
+    search_stmt = "SELECT * " \
+                  "FROM Actor " \
+                  "WHERE actor_name " \
+                  "LIKE ? " \
+                  "LIMIT 30"
+    cursor.execute(search_stmt, ('%' + name + '%',))
     return cursor.fetchall()
 
-def get_movieID(title: str) -> int:
-    cursor.execute("SELECT movie_id "
-                   "FROM Movie "
-                   "WHERE title = %s", (title,))
+
+def get_movieID(title: str) -> int | None:
+    # cursor.execute("SELECT movie_id "
+    #                "FROM Movie "
+    #                "WHERE title = %s", (title,))
+    stmt = "SELECT movie_id " \
+           "FROM Movie " \
+           "WHERE title = ?"
+    cursor.execute(stmt, (title,))
     return cursor.fetchone()[0]
