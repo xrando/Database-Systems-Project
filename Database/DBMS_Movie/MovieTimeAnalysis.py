8c2bf089a9f70f376687f9e3ac72d9bd52bb5b8b
devoalda <linuxes_mojoworld@aleeas.com>
2023-06-29 09:04:04 +0800

    feature(Time Analysis):
    Modified Time Analysis for Movie Page
    
    Signed-off-by: devoalda <linuxes_mojoworld@aleeas.com>

--- Database/DBMS_Movie/MovieTimeAnalysis.py
+++ Database/DBMS_Movie/MovieTimeAnalysis.py
@@ -1,8 +1,9 @@
-import time
-import requests
 import random
+import time
+
+import requests
+
 from Config.ConfigManager import ConfigManager
-from pprint import pprint
 
 config_manager = ConfigManager()
 config = config_manager.get_config()
@@ -17,20 +18,33 @@ movies = [
 movie_page_arr = []
 movie_details_arr = []
 
+# Movie page
 for i in range(500):
-    # random int between 1 and 100
+    # random int between 1 and 100 for page number
     page = random.randint(1, 100)
     movie_page = f'http://{host}:{port}/page/{page}'
-    # Create a GET request to the movie page, and time it
+
+    # Start timer
     start_time = time.time()
+
+    # Create a GET request to the movie page, and time it
     response = requests.get(movie_page)
+
+    # End timer
     end_time = time.time()
+
+    # Append time taken to load page to array
     movie_page_arr.append(end_time - start_time)
+
     print(f"Time taken to load page {page}: {end_time - start_time} seconds")
     # write to csv where first column is page number, second column is time taken to load page
     with open('movie_page.csv', 'a') as f:
         f.write(f"{page},{end_time - start_time}\n")
 
+    # Sleep for random time between 0.5 and 1.5 seconds
+    time.sleep(random.uniform(0.5, 1.5))
+
+# Movie details page
 for movie in movies:
     movie_details = f'http://{host}:{port}/movie/{movie}'
     start_time = time.time()
