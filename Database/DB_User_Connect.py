9ee09743b457caded56ded3dfe75a56c9d428b6f
xypoon <xiangyuanpoon@gmail.com>
2023-07-04 20:24:39 +0800

    Updated User DB to instance connection

---
+++ Database/DB_User_Connect.py
@@ -0,0 +1,99 @@
+####################################################################################################
+# This class is to create and maintain a connection to the database. It is a singleton class,      #
+# meaning that only one instance of this class can exist at a time. This is to prevent multiple    #
+# connections to the database being created.                                                       #
+####################################################################################################
+import configparser
+import sys
+import mariadb
+import logging
+
+
+class UserDBConnection:
+    """
+    Singleton class to create a connection to the database
+    """
+    _instance = None
+
+    def __new__(cls) -> object:
+        """
+        Create a new instance of the class if it doesn't exist, otherwise return the existing instance
+        """
+        if not cls._instance:
+            cls._instance = super().__new__(cls)
+            cls._instance.connection = cls._instance._create_connection()
+            cls._instance.cursor = cls._instance.connection.cursor()
+        return cls._instance
+
+    def _create_connection(self) -> mariadb.Connection:
+        """
+        Create a connection to the database
+
+        :return: A connection to the database
+        :rtype: mariadb.Connection
+        """
+        from Config.ConfigManager import ConfigManager
+
+        config_manager = ConfigManager()
+        config = config_manager.get_config()
+
+        try:
+            user = config.get('DBMS_USER', 'USERNAME')
+            password = config.get('DBMS_USER', 'PASSWORD')
+            host = config.get('DBMS_USER', 'HOST')
+            port = int(config.get('DBMS_USER', 'PORT'))
+            database = config.get('DBMS_USER', 'DATABASE')
+        except configparser.Error as e:
+            print(f"Available Configurations: {config.sections()}")
+            logging.error(f"Error reading config file: {e}")
+            sys.exit(1)
+
+        try:
+            connection = mariadb.connect(
+                user=user,
+                password=password,
+                host=host,
+                port=port,
+                database=database
+            )
+            return connection
+        except mariadb.ProgrammingError as e:
+            logging.info(f"Error connecting to MariaDB Platform: {e}, attempting to create database")
+            print(f"You don't have a database named {database} in your MariaDB instance, creating it now...")
+            try:
+                connection = mariadb.connect(
+                    user=user,
+                    password=password,
+                    host=host,
+                    port=port
+                )
+                cursor = connection.cursor()
+                cursor.execute(f"CREATE DATABASE IF NOT EXISTS {database}")
+                cursor.execute(f"USE {database}")
+
+                def create_tables(self) -> None:
+                    create_user = "CREATE TABLE IF NOT EXISTS User (" \
+                                  "id INT auto_increment NOT NULL, " \
+                                  "username VARCHAR(255) NOT NULL, " \
+                                  "password VARCHAR(255) NOT NULL, " \
+                                  "profilename VARCHAR(255) NOT NULL, " \
+                                  "email VARCHAR(255) NOT NULL, " \
+                                  "dob DATE NOT NULL, " \
+                                  "PRIMARY KEY (id, username));"
+
+                logging.info(f"Successfully created database {database} and seeded it with data")
+                return connection
+            except mariadb.Error as e:
+                logging.error(f"Error creating database: {e}")
+                sys.exit(1)
+        except mariadb.OperationalError as e:
+            logging.error(f"Error connecting to MariaDB Platform: {e}")
+            sys.exit(1)
+
+    def close_connection(self) -> None:
+        """
+        Close the connection to the database
+        :return: None
+        """
+        self.cursor.close()
+        self.connection.close()
--- Database/User.py
+++ Database/User.py
@@ -1,137 +1,53 @@
 import mariadb
-import sys
-from Config.ConfigManager import ConfigManager
+from Database.DB_User_Connect import UserDBConnection
 
+# DBMS_Movie DB Connection
+connection = UserDBConnection().connection
+cursor = connection.cursor()
 
 class Database:
-
-    def __init__(self) -> None:
-        """
-        Database Constructor, connects to the database and creates the tables if they do not exist
-
-        All Configurations are stored in the config.ini file in path ../Config/config.ini
-        """
-        # Initialize the config manager
-        config_manager = ConfigManager()
-
-        # Get the configuration
-        config = config_manager.get_config()
-
-        user = config.get('DBMS_USER', 'USERNAME')
-        password = config.get('DBMS_USER', 'PASSWORD')
-        host = config.get('DBMS_USER', 'HOST')
-        port = int(config.get('DBMS_USER', 'PORT'))
-        database = config.get('DBMS_USER', 'DATABASE')
-
-        try:
-            self.connection = mariadb.connect(user=user,
-                                              password=password,
-                                              host=host,
-                                              port=port,
-                                              database=database)
-
-            self.cursor = self.connection.cursor()
-            print("[+] Connected to Database")
-        except mariadb.ProgrammingError as e:
-            # Try to create database
-            try:
-                self.connection = mariadb.connect(user=user,
-                                                  password=password,
-                                                  host=host,
-                                                  port=port)
-                self.cursor = self.connection.cursor()
-                self.cursor.execute("CREATE DATABASE " + database)
-                self.cursor.execute("USE " + database)
-                self.create_tables()
-                print("[+] Created Database and Tables")
-            except mariadb.Error as e:
-                print(f"Error connecting to MariaDB Platform: {e}")
-                sys.exit(1)
-
-    def create_tables(self) -> None:
-        create_user = "CREATE TABLE IF NOT EXISTS User (" \
-                      "id INT auto_increment NOT NULL, " \
-                      "username VARCHAR(255) NOT NULL, " \
-                      "password VARCHAR(255) NOT NULL, " \
-                      "profilename VARCHAR(255) NOT NULL, " \
-                      "email VARCHAR(255) NOT NULL, " \
-                      "dob DATE NOT NULL, " \
-                      "PRIMARY KEY (id, username));"
-
-        # Create the tables in the database
-        self.cursor.execute(create_user)
-
-    def seed(self, seed_file: str = None) -> None:
-        """
-        Seeds the database with the data from the seed_file
-
-        :param seed_file: The file to seed the database with
-        :type seed_file: str
-        :return: None
-        """
-        if seed_file is None:
-            raise ValueError("Please provide a seed file (seed_file='path/to/file.sql')")
-
-        # Connect to the database
-        try:
-            self.cursor.execute("USE DBMS_User")
-        except mariadb.Error as e:
-            print(f"Error connecting to MariaDB Platform: {e}")
-            sys.exit(1)
-
-        # Open the seed file
-        with open(seed_file, 'r') as f:
-            # Execute the seed file
-            try:
-                self.cursor.execute(f.read())
-            except mariadb.Error as e:
-                print(f"Error executing seed file: {e}")
-                sys.exit(1)
-        # Commit the changes
-        self.connection.commit()
-
     # User functions
     def get_user_by_id(self, id: int) -> tuple:
         try:
-            self.cursor.execute("SELECT * FROM User WHERE id = ?", (id,))
+            cursor.execute("SELECT * FROM User WHERE id = ?", (id,))
         except mariadb.DataError as e:
             print(f"[-] Error retrieving user from database\n {e}")
-        return self.cursor.fetchone()
+        return cursor.fetchone()
 
     def get_password_by_username(self, username: str) -> tuple:
         try:
-            self.cursor.execute("SELECT id, password FROM User WHERE username = ?", (username,))
+            cursor.execute("SELECT id, password FROM User WHERE username = ?", (username,))
         except mariadb.DataError as e:
             print(f"[-] Error retrieving user from database\n {e}")
-        return self.cursor.fetchone()
+        return cursor.fetchone()
 
     def check_username_exists(self, username: str) -> bool:
         try:
-            self.cursor.execute("SELECT id FROM User WHERE username = ?", (username,))
+            cursor.execute("SELECT id FROM User WHERE username = ?", (username,))
         except mariadb.DataError as e:
             print(f"[-] Error retrieving user from database\n {e}")
-        return self.cursor.fetchone() is not None
+        return cursor.fetchone() is not None
 
     def create_user(self, username: str, password: str, profilename: str, email: str, dob: str) -> None:
         try:
-            self.cursor.execute("INSERT INTO User (username, password, profilename, email, dob) VALUES (?, ?, ?, ?, ?)",
+            cursor.execute("INSERT INTO User (username, password, profilename, email, dob) VALUES (?, ?, ?, ?, ?)",
                                 (username, password, profilename, email, dob))
         except mariadb.DataError as e:
             print(f"[-] Error creating user in database\n {e}")
-        self.connection.commit()
+        connection.commit()
 
     def update_user(self, id: int, username: str, password: str, profilename: str, email: str, dob: str) -> None:
         try:
-            self.cursor.execute(
+            cursor.execute(
                 "UPDATE User SET username = ?, password = ?, profilename = ?, email = ?, dob = ? WHERE id = ?",
                 (username, password, profilename, email, dob, id))
         except mariadb.DataError as e:
             print(f"[-] Error updating user in database\n {e}")
-        self.connection.commit()
+        connection.commit()
 
     def search_user(self, name: str) -> tuple:
         try:
-            self.cursor.execute("SELECT * "
+            cursor.execute("SELECT * "
                                 "FROM User "
                                 "WHERE profilename "
                                 "LIKE ?"
@@ -139,26 +55,17 @@ class Database:
                                 "LIMIT 30", ('%' + name + '%',))
         except mariadb.DataError as e:
             print(f"[-] Error searching for users from database\n {e}")
-        return self.cursor.fetchall()
+        return cursor.fetchall()
 
     def delete_user(self, id: int) -> None:
         try:
-            self.cursor.execute("DELETE FROM User WHERE id = ?", (id,))
+            cursor.execute("DELETE FROM User WHERE id = ?", (id,))
         except mariadb.DataError as e:
             print(f"[-] Error deleting user from database\n {e}")
-        # self.connection.commit()
+        # connection.commit()
 
     def manual_commit(self) -> None:
-        self.connection.commit()
+        connection.commit()
 
     def manual_rollback(self) -> None:
-        self.connection.rollback()
-
-
-if __name__ == "__main__":
-    db = Database()
-    # Retrieving user
-    # user = db.get_password_by_username("admin")
-    # print(user)
-    # user = db.get_user_by_id(2200559)
-    # print(user)
+        connection.rollback()
