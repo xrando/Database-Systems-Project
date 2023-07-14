9d6f210091cd56822bd2d12c3df956542063ab1d
xypoon <xiangyuanpoon@gmail.com>
2023-07-14 16:43:35 +0800

    Pushed Error 404 page

--- app.py
+++ app.py
@@ -26,6 +26,18 @@ def load_user(user_id):
     return None
 
 
+# Error Site Route
+@app.route('/404')
+def error404():
+    return render_template('404.html'), 404
+
+
+# Error handling page for not found sites / locations
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template('404.html'), 404
+
+
 if __name__ == '__main__':
     app.run(
         host=config.get('FLASK', 'HOST'),
